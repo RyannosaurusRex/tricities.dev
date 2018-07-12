50fbd18ab3ac74b7064c601c477480e44ccd1538
Ryan Hayes <ryan@ryanhayes.net>
2018-07-12 08:23:50 -0400

    Fixed Netlify form attributes.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,7 @@ const Contact = (props) => (
     <section id="contact">
         <div className="inner">
             <section>
-                <form method="post" action="#" netlify>
+                <form name="contact" netlify>
                     <div className="field half first">
                         <label htmlFor="name">Name</label>
                         <input type="text" name="name" id="name" />
