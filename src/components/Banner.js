b31ed52c1c84dd39763a7bfd410e9cd498e74a5a
Ryan Hayes <ryan@ryanhayes.net>
2018-07-11 22:38:07 -0400

    Initial commit/update for TriDev site.

---
+++ src/components/Banner.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Banner = (props) => (
+    <section id="banner" className="major">
+        <div className="inner">
+            <header className="major">
+                <h1>TriDevs</h1>
+            </header>
+            <div className="content">
+                <p>We're a collective of Developers, Designers, 
+                    and Entrepreneurs in Kingsport, 
+                    Johnson City, and Bristol, Tennessee</p>
+                <ul className="actions">
+                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                </ul>
+            </div>
+        </div>
+    </section>
+)
+
+export default Banner
---
+++ src/components/Contact.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+const Contact = (props) => (
+    <section id="contact">
+        <div className="inner">
+            <section>
+                <form method="post" action="#">
+                    <div className="field half first">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+                    <div className="field half">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="6"></textarea>
+                    </div>
+                    <ul className="actions">
+                        <li><input type="submit" value="Send Message" className="special" /></li>
+                        <li><input type="reset" value="Clear" /></li>
+                    </ul>
+                </form>
+            </section>
+            <section className="split">
+                <section>
+                    <div className="contact-method">
+                        <span className="icon alt fa-slack"></span>
+                        <h3>Join us on Slack!</h3>
+                        <a href="https://tridevs.herokuapp.com">TriDevs Slack</a>
+                    </div>
+                </section>
+                <section>
+                    <div className="contact-method">
+                        <span className="icon alt fa-group"></span>
+                        <h3><a href="https://meetup.com/tridev">Meetup.com</a></h3>
+                    </div>
+                </section>
+                <section>
+                    <div className="contact-method">
+                        <span className="icon alt fa-home"></span>
+                        <h3>Address</h3>
+                        <span>1234 Somewhere Road #5432<br />
+                        Nashville, TN 00000<br />
+                        United States of America</span>
+                    </div>
+                </section>
+            </section>
+        </div>
+    </section>
+)
+
+export default Contact
