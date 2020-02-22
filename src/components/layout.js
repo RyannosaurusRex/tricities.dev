72b66e28c5fde384170f9f3990d68448bd358f5b
Ryan Hayes <ryan@ryanhayes.net>
2020-02-22 10:15:31 -0500

    Removed privacy link since we don't collect/use anything.

--- src/components/layout.js
+++ src/components/layout.js
@@ -416,9 +416,9 @@ function Layout({ children }) {
                                 <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                                     <a href="https://jetbrains.com" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Jetbrains</a>
                                 </li>
-                                <li className="mt-2 inline-block mr-2 md:block md:mr-0">
+                                {/* <li className="mt-2 inline-block mr-2 md:block md:mr-0">
                                     <a href="#" className="no-underline hover:underline text-gray-800 hover:text-orange-500">Privacy</a>
-                                </li>
+                                </li> */}
                             </ul>
                         </div>
                         {/* <div className="flex-1">
